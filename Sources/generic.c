288b68e1fc4baf1795c37b668bcaf6ea613f2b2c
Tom <tom-vautray@hotmail.fr>
2020-05-08 17:50:36 +0200

    Admin WIP2
    +added _mkdir from https://stackoverflow.com/questions/2336242/recursive-mkdir-system-call-on-unix

--- Sources/generic.c
+++ Sources/generic.c
@@ -114,6 +114,24 @@ int random(int min, int max)
 	}
 }
 
+static void _mkdir(const char *dir) {
+        char tmp[256];
+        char *p = NULL;
+        size_t len;
+
+        snprintf(tmp, sizeof(tmp),"%s",dir);
+        len = strlen(tmp);
+        if(tmp[len - 1] == '/')
+                tmp[len - 1] = 0;
+        for(p = tmp + 1; *p; p++)
+                if(*p == '/') {
+                        *p = 0;
+                        mkdir(tmp, S_IRWXU);
+                        *p = '/';
+                }
+        mkdir(tmp, S_IRWXU);
+}
+
 int mkSdir(char *path, bool includeIfPoint)
 {
 	if(sizeof(path) > 1024)
